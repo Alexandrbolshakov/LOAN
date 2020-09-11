3bd9c153661d15d6dc847c510b6103e71d195143
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-09-11 16:34:48 +0300

    add difference

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from './modules/slider/slider-main';
 import VideoPlayer from './modules/playVideo';
 import MiniSlider from './modules/slider/slider-mini';
+import Difference from './modules/difference';
 
 window.addEventListener('DOMContentLoaded', ()=>{
     const mainSlider = new MainSlider({container: '.page', btns: '.next'});
@@ -36,4 +37,8 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    const difference = new Difference('.officerold', '.officernew', '.officer__card-item');
+
+    difference.init();
 });
